6d35f69aab980ae15c738d6b3efa38031577f1e0
wodls1616 <wodls1616@gmail.com>
2023-10-10 12:54:39 +0900

    Querydsl에서 동적쿼리를 하는 방법은 BooleanBuilder와 where 다중 파라미터 방식으로 만들수 있다. BooleanBuilder를 사용하게 될 경우 파라미터의 값이 null이면 자동으로 queryFactory                 .selectFrom(member)                 .where(builder)                 .fetch();에서 무시하도 쿼리가 작동하고 있으면 값을 넣어서 쿼리를 실행시킨다. 만약 어떤 파라미터가 필수 값이면 BooleanBuilder() 생성자 안에 파라미터를 입력해주면 된다.(        BooleanBuilder builder = new BooleanBuilder(member.username.eq(usernameCond); ) 이런식으로 넣어주면 된다.

--- src/test/java/study/querydsl/QueryBasicTest.java
+++ src/test/java/study/querydsl/QueryBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -544,5 +545,30 @@ public class QueryBasicTest {
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
         }
+    }
+
+    @Test
+    public void dynamicQuery_BooleanBuilder() {
+        String usernameParam = "member1";
+        Integer ageParam = null;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+         return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
     }
 }
