ab6ca6c0c31e9644f5a29fa7bf54ae4b18b19681
wodls1616 <wodls1616@gmail.com>
2023-10-10 14:10:06 +0900

    Querydsl에서 SQL Function을 호출하기 위해서는 Expressions.stringTemplate를 사용해서 호출하면 된다. 하지만 기본적인 ansi표준은 이미 Querydsl이 지원하므로 그걸 사용하고 직접 만든 Function일 경우 등록 후 Expressions.stringTemplated을 사용하도록 하자

--- src/test/java/study/querydsl/QueryBasicTest.java
+++ src/test/java/study/querydsl/QueryBasicTest.java
@@ -640,4 +640,31 @@ public class QueryBasicTest {
                 .set(member.age, member.age.add(1))
                 .execute();
     }
+
+    @Test
+    public void sqlFunction() {
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace',{0},{1},{2})", member.username, "member", "M"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void sqlFunction2() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                //.where(member.username.eq(Expressions.stringTemplate("function('lower',{0})", member.username)))
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 }
